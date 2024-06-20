045274cbb8a4c3a3331d8ff98a9dc94e857e11e1
Saurabh Prakash <saurabhp75@gmail.com>
2024-06-20 07:42:54 +0530

    Remove searchbar

--- app/routes/_site-layout.tsx
+++ app/routes/_site-layout.tsx
@@ -1,6 +1,5 @@
-import { Link, Outlet, useMatches, useRouteLoaderData } from '@remix-run/react'
+import { Link, Outlet, useRouteLoaderData } from '@remix-run/react'
 import { Logo } from '#app/components/logo.js'
-import { SearchBar } from '#app/components/search-bar.js'
 import { Button } from '#app/components/ui/button.js'
 import { UserDropdown } from '#app/components/user-dropdown.js'
 import { type loader as rootLoader } from '#app/root.tsx'
@@ -10,20 +9,14 @@ import { ThemeSwitch } from './resources+/theme-switch'
 export default function SiteLayout() {
 	const data = useRouteLoaderData<typeof rootLoader>('root')
 	const user = useOptionalUser()
-	const matches = useMatches()
-	const isOnSearchPage = matches.find(
-		(m) => m.id === 'routes/_site-layout+/users+/index',
-	)
-	const searchBar = isOnSearchPage ? null : <SearchBar status="idle" />
+
 	return (
 		// <div className="flex h-screen flex-col border-red-600 border-4">
 		<div className="flex h-screen flex-col">
 			<header className="container py-6">
 				<nav className="flex flex-wrap items-center justify-between gap-4 sm:flex-nowrap md:gap-8">
 					<Logo />
-					<div className="ml-auto hidden max-w-sm flex-1 sm:block">
-						{searchBar}
-					</div>
+
 					<div className="flex items-center gap-10">
 						{user ? (
 							<UserDropdown />
